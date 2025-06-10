36afa6071f750d5c42bdad40485f2b97ae96f8d0
Vel2025 <velbalaiya@gmail.com>
2025-06-09 23:16:26 -0600

    Code updated and working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,13 @@ function Home() {
   return (
     <div>
       <h1>Welcome to the Clinical Trial Management System</h1>
+      <h2> See how Trail App accelerates clinical trials</h2>
       <p>Manage clinical trials efficiently with our secure platform.</p>
+      <ul>
+        <li>Gain a Full View of Your Trial</li>
+        <li>Enhance Medical Care and Attention</li>
+        <li>Contribute to Medical Advancement</li>
+      </ul>
     </div>
   );
 }
